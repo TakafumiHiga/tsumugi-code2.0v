9cf0b98dcde81a6a37e0744d70a3cfe92fb4ed2c
TakafumiHiga <taka239133@icloud.com>
2024-01-09 05:55:31 +0900

    sliderページ追加

---
+++ WordPressTheme/includes/kv-slider.php
@@ -0,0 +1,25 @@
+<section class="l-mv u-bg-color-navy">
+  <div class="p-mv">
+    <div class="p-mv__img swiper topSwiper">
+      <div class="swiper-wrapper">
+        <div class="swiper-slide">
+          <img src="<?php echo esc_url(get_theme_file_uri('/assets/images/page-slider/mv-slider1.jpg')); ?>" alt="">
+        </div>
+        <div class="swiper-slide">
+          <img src="<?php echo esc_url(get_theme_file_uri('/assets/images/page-slider/mv-slider2.jpg')); ?>" alt="">
+        </div>
+        <div class="swiper-slide">
+          <img src="<?php echo esc_url(get_theme_file_uri('/assets/images/page-slider/mv-slider3.jpg')); ?>" alt="">
+        </div>
+      </div>
+    </div>
+    <div class="p-mv__text-wrap">
+      <p class="p-mv__text">10年<span class="p-mv__text--small">以上の</span><span
+          class="p-mv__text--highlight">契約継続率85<span class="p-mv__text--small">%</span></span></p>
+      <p class="p-mv__text">
+        産婦人科給食<span class="p-mv__text--small">の</span></p>
+      <p class="p-mv__text"><span class="p-mv__text--mid">リーディングカンパニー</span></p>
+      <p class="p-mv_catch-copy">おいしさとやさしさを、ずっと。</p>
+    </div>
+  </div>
+</section>
